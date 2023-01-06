5ca2c88234658f4142e406fec806ff12dc200a65
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-01-06 21:43:20 +0300

    Add files via upload

---
+++ sql dvdrental 3.sql
@@ -0,0 +1,12 @@
+        --1--
+select  country from country
+where country like 'A%a';
+        --2--
+select  country from country
+where country like '_____%n';
+        --3--
+select title from film
+where title ilike '%t%t%t%t%';
+        --4--
+select *from film
+where title like 'C%' and length>90 and rental_rate =2.99;
