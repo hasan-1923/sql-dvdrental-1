ffe6c537ab0753cf112e688f0314e7133d810936
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-01-17 00:12:55 +0300

    Add files via upload

---
+++ dvdrental-hw-having.sql
@@ -0,0 +1,21 @@
+--1--
+SELECT rating FROM film
+GROUP BY rating;
+
+--2--
+
+SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+--3--
+
+SELECT store_id,COUNT(customer_id) FROM customer
+GROUP BY store_id;
+
+--4--
+
+SELECT country_id,COUNT(country_id) FROM city
+GROUP BY country_id 
+ORDER BY COUNT(country_id) DESC
+LIMIT 1;
