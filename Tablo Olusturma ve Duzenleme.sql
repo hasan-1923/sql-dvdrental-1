e04c4ccef4ee51cf30342a157e4fc18eb6d778fa
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-01-19 19:36:34 +0300

    Add files via upload

---
+++ Tablo Olusturma ve Duzenleme.sql
@@ -0,0 +1,151 @@
+--Oluşturma--
+create table employee (
+	id serial,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+insert into employee (id, first_name, last_name) values (1, 'Faunie', 'Danigel');
+insert into employee (id, first_name, last_name) values (2, 'Marni', 'Tippetts');
+insert into employee (id, first_name, last_name) values (3, 'Gillie', 'Dutton');
+insert into employee (id, first_name, last_name) values (4, 'Selie', 'Vannozzii');
+insert into employee (id, first_name, last_name) values (5, 'Lutero', 'McGavigan');
+insert into employee (id, first_name, last_name) values (6, 'Gene', 'Sauvan');
+insert into employee (id, first_name, last_name) values (7, 'Caryl', 'Roll');
+insert into employee (id, first_name, last_name) values (8, 'Orelee', 'Cubbino');
+insert into employee (id, first_name, last_name) values (9, 'Ferris', 'Feely');
+insert into employee (id, first_name, last_name) values (10, 'Melody', 'Croisdall');
+insert into employee (id, first_name, last_name) values (11, 'Rivy', 'Menere');
+insert into employee (id, first_name, last_name) values (12, 'Crystie', 'Wilford');
+insert into employee (id, first_name, last_name) values (13, 'Amara', 'Giannini');
+insert into employee (id, first_name, last_name) values (14, 'Lennie', 'Poyntz');
+insert into employee (id, first_name, last_name) values (15, 'Lawrence', 'Winkell');
+insert into employee (id, first_name, last_name) values (16, 'Maison', 'Crebo');
+insert into employee (id, first_name, last_name) values (17, 'Veronique', 'Gurko');
+insert into employee (id, first_name, last_name) values (18, 'Gwendolen', 'Edrich');
+insert into employee (id, first_name, last_name) values (19, 'Lucias', 'Stearn');
+insert into employee (id, first_name, last_name) values (20, 'Dona', 'Ebdin');
+insert into employee (id, first_name, last_name) values (21, 'Tillie', 'Mundle');
+insert into employee (id, first_name, last_name) values (22, 'Val', 'Dunsmuir');
+insert into employee (id, first_name, last_name) values (23, 'Guilbert', 'Lawful');
+insert into employee (id, first_name, last_name) values (24, 'Daron', 'Sandcroft');
+insert into employee (id, first_name, last_name) values (25, 'Sandy', 'Lusk');
+insert into employee (id, first_name, last_name) values (26, 'Brittne', 'Rolfi');
+insert into employee (id, first_name, last_name) values (27, 'Arly', 'Baffin');
+insert into employee (id, first_name, last_name) values (28, 'Sydney', 'Whitwam');
+insert into employee (id, first_name, last_name) values (29, 'Victor', 'Pallister');
+insert into employee (id, first_name, last_name) values (30, 'Lauretta', 'Bilbey');
+insert into employee (id, first_name, last_name) values (31, 'Lyndel', 'Doubrava');
+insert into employee (id, first_name, last_name) values (32, 'Demetrius', 'Swales');
+insert into employee (id, first_name, last_name) values (33, 'Judi', 'Janata');
+insert into employee (id, first_name, last_name) values (34, 'Giorgio', 'Cossam');
+insert into employee (id, first_name, last_name) values (35, 'Lyndsey', 'Sweett');
+insert into employee (id, first_name, last_name) values (36, 'See', 'Couve');
+insert into employee (id, first_name, last_name) values (37, 'Ddene', 'Waleran');
+insert into employee (id, first_name, last_name) values (38, 'Harlan', 'Grocott');
+insert into employee (id, first_name, last_name) values (39, 'Augusto', 'Alen');
+insert into employee (id, first_name, last_name) values (40, 'Donall', 'Jeenes');
+insert into employee (id, first_name, last_name) values (41, 'Lelia', 'Monk');
+insert into employee (id, first_name, last_name) values (42, 'Ediva', 'Kedslie');
+insert into employee (id, first_name, last_name) values (43, 'Nelle', 'Beek');
+insert into employee (id, first_name, last_name) values (44, 'Andra', 'Marryatt');
+insert into employee (id, first_name, last_name) values (45, 'Verile', 'Kensett');
+insert into employee (id, first_name, last_name) values (46, 'Bail', 'Butrimovich');
+insert into employee (id, first_name, last_name) values (47, 'Marissa', 'Cram');
+insert into employee (id, first_name, last_name) values (48, 'Meta', 'Pigden');
+insert into employee (id, first_name, last_name) values (49, 'Agnesse', 'Bamell');
+insert into employee (id, first_name, last_name) values (50, 'Dal', 'Bendin');
+insert into employee (id, first_name, last_name) values (51, 'Wendall', 'Lebbern');
+insert into employee (id, first_name, last_name) values (52, 'Karim', 'Cleife');
+insert into employee (id, first_name, last_name) values (53, 'Diana', 'Camsey');
+insert into employee (id, first_name, last_name) values (54, 'Janeen', 'Redemile');
+insert into employee (id, first_name, last_name) values (55, 'Belia', 'Bann');
+insert into employee (id, first_name, last_name) values (56, 'Waneta', 'Bushe');
+insert into employee (id, first_name, last_name) values (57, 'Marge', 'Drable');
+insert into employee (id, first_name, last_name) values (58, 'Dredi', 'Perdue');
+insert into employee (id, first_name, last_name) values (59, 'Ophelia', 'Whight');
+insert into employee (id, first_name, last_name) values (60, 'Freeman', 'Miquelet');
+insert into employee (id, first_name, last_name) values (61, 'Walt', 'Brompton');
+insert into employee (id, first_name, last_name) values (62, 'Winthrop', 'Sample');
+insert into employee (id, first_name, last_name) values (63, 'Tracy', 'Tomblin');
+insert into employee (id, first_name, last_name) values (64, 'Noelle', 'Alwell');
+insert into employee (id, first_name, last_name) values (65, 'Mareah', 'Hek');
+insert into employee (id, first_name, last_name) values (66, 'Darlene', 'Hoopper');
+insert into employee (id, first_name, last_name) values (67, 'Corly', 'Sindle');
+insert into employee (id, first_name, last_name) values (68, 'Morgana', 'Bossom');
+insert into employee (id, first_name, last_name) values (69, 'Scott', 'Ramirez');
+insert into employee (id, first_name, last_name) values (70, 'Elly', 'O''Hare');
+insert into employee (id, first_name, last_name) values (71, 'Verile', 'Smalridge');
+insert into employee (id, first_name, last_name) values (72, 'Bank', 'Petracci');
+insert into employee (id, first_name, last_name) values (73, 'Aloysius', 'Hazeldean');
+insert into employee (id, first_name, last_name) values (74, 'Sadye', 'Farley');
+insert into employee (id, first_name, last_name) values (75, 'Bernice', 'Gregolin');
+insert into employee (id, first_name, last_name) values (76, 'Marion', 'Hardington');
+insert into employee (id, first_name, last_name) values (77, 'Nita', 'Innocent');
+insert into employee (id, first_name, last_name) values (78, 'Sorcha', 'Gartan');
+insert into employee (id, first_name, last_name) values (79, 'Kata', 'Slayny');
+insert into employee (id, first_name, last_name) values (80, 'Georgie', 'Sabathier');
+insert into employee (id, first_name, last_name) values (81, 'Meggy', 'Oldershaw');
+insert into employee (id, first_name, last_name) values (82, 'Min', 'Woolrich');
+insert into employee (id, first_name, last_name) values (83, 'Malva', 'Idenden');
+insert into employee (id, first_name, last_name) values (84, 'Lowe', 'Faulks');
+insert into employee (id, first_name, last_name) values (85, 'Sybyl', 'Arnoldi');
+insert into employee (id, first_name, last_name) values (86, 'Wallas', 'Blazek');
+insert into employee (id, first_name, last_name) values (87, 'Mariquilla', 'Adamovitz');
+insert into employee (id, first_name, last_name) values (88, 'Waly', 'Heaffey');
+insert into employee (id, first_name, last_name) values (89, 'Annmaria', 'Hartus');
+insert into employee (id, first_name, last_name) values (90, 'Debbi', 'Hildred');
+insert into employee (id, first_name, last_name) values (91, 'Pippa', 'Hammond');
+insert into employee (id, first_name, last_name) values (92, 'Burty', 'Mattek');
+insert into employee (id, first_name, last_name) values (93, 'Alfi', 'Cohrs');
+insert into employee (id, first_name, last_name) values (94, 'Vannie', 'Moores');
+insert into employee (id, first_name, last_name) values (95, 'Emmye', 'MacGowan');
+insert into employee (id, first_name, last_name) values (96, 'Fraze', 'Hanby');
+insert into employee (id, first_name, last_name) values (97, 'Fleur', 'Duffus');
+insert into employee (id, first_name, last_name) values (98, 'Thelma', 'Denham');
+insert into employee (id, first_name, last_name) values (99, 'Jaquelyn', 'Bellhouse');
+insert into employee (id, first_name, last_name) values (100, 'Bale', 'Hansman');
+
+
+
+
+------ Güncelleme---
+update employee
+set first_name='Hasan'
+where id =1;
+
+update employee
+set first_name='Kaan'
+where id =2;
+
+update employee
+set first_name='Oğuz'
+where id =3;
+
+update employee
+set first_name='Tuğkan'
+where id =4;
+
+update employee
+set first_name='Çağlar'
+where id =5;
+------------------------ Silme-----------------
+
+delete from employee
+where id = 100 ;
+ 
+delete from employee
+where id = 99;
+ 
+delete from employee 
+where first_name ='Val';
+
+  
+delete from employee 
+where last_name='Baffin';
+ 
+ 
+delete from employee
+where id= 65;
+  
+delete from employee 
+where first_name ='See'
